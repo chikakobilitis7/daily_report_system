70c65a684ada46e7c3ab288fb7e1e4e0eb29b24d
chikakobilitis7 <brilliantsinger.c16@gmail.com>
2020-03-30 18:22:02 +0900

    Rename Aaa.java to LogoutServlet.java

--- src/controllers/login/LogoutServlet.java
+++ src/controllers/login/LogoutServlet.java
@@ -12,13 +12,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class LogoutServlet
  */
 @WebServlet("/logout")
-public class Aaa extends HttpServlet {
+public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Aaa() {
+    public LogoutServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
